c7986323ee74bdb1277817f6c5acd129232fbc5c
vansh46 <78260154+vansh46@users.noreply.github.com>
2021-02-16 20:34:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var fixedRect,movingRect
+function setup() {
+  createCanvas(800,400);
+fixedRect=createSprite(400, 300, 50, 50);
+fixedRect.shapeColor = "blue";
+movingRect=createSprite(400, 100, 80, 30);
+movingRect.shapeColor = "blue";
+movingRect.velocityY = 2;
+fixedRect.velocityY = -2;
+
+}
+
+function draw() {
+  background(0);  
+  //movingRect.x = mouseX;
+ // movingRect.y = mouseY;
+  bounceOff(movingRect,fixedRect);
+ 
+
+  drawSprites();
+
+}
+function bounceOff(object1,object2){
+  if(object1.x-object2.x<object2.width/2+object1.width/2&&object2.x-object1.x<object2.width/2+object1.width/2 ){
+
+  
+
+  
+    object1.velocityX = object1.velocityX *(-1);
+     
+    object2.velocityX = object2.velocityX *(-1);
+
+     
+     }
+    if ( object1.y-object2.y<object2.height/2+object1.height/2&&object2.y-object1.y<object2.height/2+object1.height/2){
+      object1.velocityY = object1.velocityY *(-1);
+     
+      object2.velocityY = object2.velocityY *(-1);
+   
+    }
+
+
+  }
